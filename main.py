e141eed9bf547bf45108a93ecbc3b4ffb41382df
Slicky325 <vansh_a@ph.iitr.ac.in>
2023-10-27 22:31:50 +0530

    added audio files and few backgrounds still setting up start

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame, sys
 pygame.init()
 
-#setting the screen up
+#setting the screen up, logo, font, music
 Main_surf = pygame.display.set_mode((1400,750))
 pygame.display.set_caption('Star wars ripoff')
-logo = pygame.image.load('logo,png')
+logo = pygame.image.load('Images\\logo.png')
 pygame.display.set_icon(logo)
 font=pygame.font.Font('pixel.ttf')
+bg_music = pygame.mixer.Sound('audio\\music.mp3')
+bg_music.play(loops = -1)
 clock = pygame.time.Clock()
-game_active=True
+game_active=False
 
 #background setup
-backg=pygame.image.load('Space Background.png').convert_alpha()
+backg=pygame.image.load('Images\\Space Background.png').convert_alpha()
 x1=1400
 x2=0
+intro_msg=font.render('Welcome to the start of the game', False, 'Green')
+intro_msg_rect=intro_msg.get_rect(center=(700,50))
 
 #player class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-
-
 
 
 
@@ -33,18 +35,21 @@ while True:
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+     
     
-    #background animantions
-    Main_surf.blit(backg,(x1,0))
-    Main_surf.blit(backg,(x2,0))
-    x1-=1
-    x2-=1
-    if x1<0: x1=1400
-    if x2<-1400: x2=0 
-    
-    if game_active:a=1
+    if game_active:
+        a=1
     else:
-        intro_msg=font.render
+        #background animations
+        Main_surf.blit(backg,(x1,0))
+        Main_surf.blit(backg,(x2,0))
+        x1-=1
+        x2-=1
+        if x1<0: x1=1400
+        if x2<-1400: x2=0
+        Main_surf.blit(intro_msg,intro_msg_rect)
+        Main_surf.blit()
+        
 
     pygame.display.update()
     clock.tick(60)
